efc46e0817c054fec71a0f638efed7404e4a1e15
7과11사이 <dongjunlee222@gmail.com>
2024-04-29 11:39:08 +0900

    [FEAT]: initial type tests created

---
+++ HA/Module/Accelerometer/Test/TypeA.swift
@@ -0,0 +1,8 @@
+//
+//  TypeA.swift
+//  HA
+//
+//  Created by Porori on 4/29/24.
+//
+
+import Foundation
---
+++ HA/Module/Accelerometer/Test/TypeB.swift
@@ -0,0 +1,8 @@
+//
+//  TypeB.swift
+//  HA
+//
+//  Created by Porori on 4/29/24.
+//
+
+import Foundation
