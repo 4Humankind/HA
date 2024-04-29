18e37f2c344fde9aee441ec5d4b4f7126fdc5ff3
7과11사이 <dongjunlee222@gmail.com>
2024-04-29 11:12:08 +0900

    Initial Setup

---
+++ HA/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  HA
+//
+//  Created by Porori on 4/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
