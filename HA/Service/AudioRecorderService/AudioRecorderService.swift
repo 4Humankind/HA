d50200cdde578119a87377e47bffd8f951a1d790
dongglehada <ghddns34@gmail.com>
2024-05-07 16:40:34 +0900

    HA-13 [FEAT]: AudioRecorderService

--- HA/Service/AudioRecorderService/AudioRecorderService.swift
+++ HA/Service/AudioRecorderService/AudioRecorderService.swift
@@ -9,55 +9,114 @@ import Foundation
 
 import AVFoundation
 
-class AudioRecorderService {
+class AudioRecorderService: NSObject {
+    // MARK: - Properties
+    private var recordingSession: AVAudioSession!
+    private var audioRecorder: AVAudioRecorder!
+    private var isRecording: Observable<Bool> = Observable(false)
     
-    var audioRecorder: AVAudioRecorder!
+    override init() {
+        super.init()
+        setup()
+    }
+}
 
-    func startRecording() {
-        // AVAudioSession의 싱글턴 인스턴스를 가져옵니다.
-        let session = AVAudioSession.sharedInstance()
-        do {
-            // 오디오 세션의 카테고리를 녹음 및 재생으로 설정합니다.
-            try session.setCategory(.playAndRecord, mode: .default)
-            // 오디오 세션을 활성화합니다.
-            try session.setActive(true)
-        } catch {
-            // 세션 설정에 실패했을 경우 에러 메시지를 출력합니다.
-            print("Failed to set up recording session")
-        }
-
-        // 녹음 파일 저장을 위한 고유한 파일 이름을 생성합니다.
-        let fileName = UUID().uuidString + ".m4a"
-        // 문서 디렉터리의 경로를 가져옵니다.
-        let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-        // 파일의 전체 경로를 생성합니다.
-        let fileURL = documentPath.appendingPathComponent(fileName)
+private extension AudioRecorderService {
+    func setup() {
+        recordingSession = AVAudioSession.sharedInstance()
         
-        // 녹음 설정을 정의합니다. AAC 포맷, 샘플 레이트 12000 Hz, 단일 채널, 높은 오디오 품질로 설정합니다.
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            print("AudioRecorderService Active Success")
+        } catch {
+            print("AudioRecorderService Active Fail")
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func getFileURL() -> URL {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        let time = dateFormatter.string(from: Date())
+        let path = getDocumentsDirectory().appendingPathComponent("recording\(time).m4a")
+        return path as URL
+    }
+}
+
+// MARK: - Methods
+extension AudioRecorderService {
+    
+    func startRecording() {
+        
+        print(#function)
+        let audioFilename = getFileURL()
+        
         let settings = [
             AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
             AVSampleRateKey: 12000,
             AVNumberOfChannelsKey: 1,
             AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
         ]
-
+        
         do {
-            // AVAudioRecorder 인스턴스를 생성하고 초기화합니다. 위에서 정의된 설정과 경로를 사용합니다.
-            audioRecorder = try AVAudioRecorder(url: fileURL, settings: settings)
-            // 미터링을 활성화하여 녹음 중 오디오 레벨을 측정할 수 있게 합니다.
-            audioRecorder.isMeteringEnabled = true
-            // 녹음 준비를 합니다.
-            audioRecorder.prepareToRecord()
-            // 녹음을 시작합니다.
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            isRecording.value = true
             audioRecorder.record()
-            
         } catch {
-            // 녹음 시작에 실패했을 경우 에러 메시지를 출력합니다.
-            print("Recording failed to start")
+            isRecording.value = false
+            print("Recording Start Fail")
         }
     }
-
+    
     func stopRecording() {
+        print(#function)
         audioRecorder.stop()
+        audioRecorder = nil
+        isRecording.value = false
+    }
+    
+    func getAllRecordingsURLs() -> [URL] {
+        let documentsDirectory = getDocumentsDirectory()
+        var recordings: [URL] = []
+        
+        do {
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsDirectory, includingPropertiesForKeys: nil)
+            for fileURL in directoryContents {
+                // .m4a 확장자를 가진 파일만 recordings 배열에 추가
+                if fileURL.pathExtension == "m4a" {
+                    recordings.append(fileURL)
+                }
+            }
+        } catch {
+            print("Error while enumerating files \(documentsDirectory.path): \(error.localizedDescription)")
+        }
+        
+        return recordings
+    }
+    
+    func subscribeIsRecording(subscribe: @escaping (Bool) -> Void) {
+        isRecording.bind { isRecording in
+            guard let isRecording = isRecording else { return }
+            subscribe(isRecording)
+        }
+    }
+}
+
+// MARK: - Delegate
+extension AudioRecorderService: AVAudioRecorderDelegate {
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        print("Recording Success")
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        print("Error while recording audio \(error!.localizedDescription)")
     }
 }
--- HA/Test/ViewController/AudioRecordTestVC.swift
+++ HA/Test/ViewController/AudioRecordTestVC.swift
@@ -15,13 +15,10 @@ class AudioRecordTestVC: UIViewController, AVAudioRecorderDelegate, AVAudioPlaye
     var recordButton = UIButton()
     var playButton = UIButton()
     
-    var recordingSession: AVAudioSession!
-    var audioRecorder: AVAudioRecorder!
-    var audioPlayer:AVAudioPlayer!
-    var isPlay: Bool = false
+    var recordService = AudioRecorderService()
     
     override func viewDidLoad() {
-        setupView()
+        setUpUI()
     }
     
     func setUpUI() {
@@ -42,158 +39,18 @@ class AudioRecordTestVC: UIViewController, AVAudioRecorderDelegate, AVAudioPlaye
             make.leading.equalTo(view.snp.centerX)
             make.centerY.equalToSuperview()
         }
-        
-        recordButton.isEnabled = true
-        playButton.isEnabled = false
         recordButton.addTarget(self, action: #selector(recordAudioButtonTapped(_:)), for: .primaryActionTriggered)
         playButton.addTarget(self, action: #selector(playAudioButtonTapped(_:)), for: .primaryActionTriggered)
     }
     
-    func setupView() {
-        recordingSession = AVAudioSession.sharedInstance()
-        
-        do {
-            try recordingSession.setCategory(.playAndRecord, mode: .default)
-            try recordingSession.setActive(true)
-            recordingSession.requestRecordPermission() { [unowned self] allowed in
-                DispatchQueue.main.async {
-                    if allowed {
-                        self.setUpUI()
-                    } else {
-                        // failed to record
-                    }
-                }
-            }
-        } catch {
-            // failed to record
-        }
-    }
     
     @objc func recordAudioButtonTapped(_ sender: UIButton) {
-        if audioRecorder == nil {
-            startRecording()
-        } else {
-            finishRecording(success: true)
-        }
-    }
-    
-    func startRecording() {
-        let audioFilename = getFileURL()
-        
-        let settings = [
-            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-            AVSampleRateKey: 12000,
-            AVNumberOfChannelsKey: 1,
-            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
-        ]
-        
-        do {
-            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
-            audioRecorder.delegate = self
-            audioRecorder.record()
-            
-            recordButton.setImage(UIImage(systemName: "record.circle.fill"), for: .normal)
-            playButton.isEnabled = false
-        } catch {
-            finishRecording(success: false)
-        }
-    }
-    
-    func finishRecording(success: Bool) {
-        audioRecorder.stop()
-        audioRecorder = nil
-        
-        if success {
-            recordButton.setImage(UIImage(systemName: "record.circle.fill"), for: .normal)
-        } else {
-            recordButton.setImage(UIImage(systemName: "record.circle"), for: .normal)
-            // recording failed :(
-        }
-        
-        playButton.isEnabled = true
-        recordButton.isEnabled = true
+        recordService.startRecording()
     }
     
     @objc func playAudioButtonTapped(_ sender: UIButton) {
-        if isPlay {
-            audioPlayer.stop()
-            sender.setImage(UIImage(systemName: "Play"), for: .normal)
-        } else {
-            recordButton.isEnabled = false
-            sender.setImage(UIImage(systemName: "stop"), for: .normal)
-            preparePlayer()
-            audioPlayer.play()
-        }
-        isPlay.toggle()
+        recordService.stopRecording()
     }
-    
-    func preparePlayer() {
-        var error: NSError?
-        do {
-            audioPlayer = try AVAudioPlayer(contentsOf: getFileURL() as URL)
-        } catch let error1 as NSError {
-            error = error1
-            audioPlayer = nil
-        }
-        
-        if let err = error {
-            print("AVAudioPlayer error: \(err.localizedDescription)")
-        } else {
-            audioPlayer.delegate = self
-            audioPlayer.prepareToPlay()
-            audioPlayer.volume = 10.0
-        }
-    }
-    
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }
-    
-    func getFileURL() -> URL {
-        let path = getDocumentsDirectory().appendingPathComponent("recording.m4a")
-        return path as URL
-    }
-    
-    //MARK: Delegates
-    
-    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
-        if !flag {
-            finishRecording(success: false)
-        }
-    }
-    
-    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
-        print("Error while recording audio \(error!.localizedDescription)")
-    }
-    
-    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-        recordButton.isEnabled = true
-        playButton.setImage(UIImage(systemName: "Play"), for: .normal)
-    }
-    
-    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
-        print("Error while playing audio \(error!.localizedDescription)")
-    }
-    
-    //MARK: To upload video on server
-    
-    func uploadAudioToServer() {
-        /*Alamofire.upload(
-         multipartFormData: { multipartFormData in
-         multipartFormData.append(getFileURL(), withName: "audio.m4a")
-         },
-         to: "https://yourServerLink",
-         encodingCompletion: { encodingResult in
-         switch encodingResult {
-         case .success(let upload, _, _):
-         upload.responseJSON { response in
-         Print(response)
-         }
-         case .failure(let encodingError):
-         print(encodingError)
-         }
-         })*/
-    }
-    
+
 }
+
