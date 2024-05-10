248cfcb4e5dbfc67ee031241d87bb2ef65485cd1
7과11사이 <dongjunlee222@gmail.com>
2024-05-10 23:25:04 +0900

    ADD: 📇 camera creation test #3

---
+++ HA/Service/CameraPreviewService/CameraPreviewService.swift
@@ -0,0 +1,8 @@
+//
+//  CameraPreviewService.swift
+//  HA
+//
+//  Created by Porori on 5/10/24.
+//
+
+import Foundation
--- HA/Test/ViewController/CameraTestVC.swift
+++ HA/Test/ViewController/CameraTestVC.swift
@@ -7,53 +7,120 @@
 
 import UIKit
 import AVFoundation
-import Photos
 import SnapKit
 
 class CameraTestVC: UIViewController {
-    
-    var cameraButton = UIButton()
-    var videoButton = UIButton()
-    var preview = UIView()
+    private var cameraButton = UIButton()
+    private var videoButton = UIButton()
+    private var imageOutput = AVCapturePhotoOutput()
+    private var captureSession: AVCaptureSession?
+    private var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        
-        setVideo()
-        setCamera()
+        setupUI()
+        checkCameraPermission()
     }
-
-    func setCamera() {
-        view.addSubview(cameraButton)
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        setupCameraButton()
+        setupVideoButton()
+    }
+    
+    private func setupCameraButton() {
         cameraButton.setImage(UIImage(systemName: "camera"), for: .normal)
-        cameraButton.backgroundColor = .blue
+        cameraButton.addTarget(self, action: #selector(cameraButtonTapped), for: .touchUpInside)
         
+        view.addSubview(cameraButton)
         cameraButton.snp.makeConstraints { make in
-            make.size.equalTo(50)
+            make.size.equalTo(100)
             make.trailing.equalTo(view.snp.trailing).inset(50)
             make.centerY.equalTo(view.snp.centerY)
         }
-        cameraButton.addTarget(self, action: #selector(cameraButtonTapped), for: .touchUpInside)
     }
     
-    func setVideo() {
-        view.addSubview(videoButton)
+    private func setupVideoButton() {
         videoButton.setImage(UIImage(systemName: "video"), for: .normal)
-        videoButton.backgroundColor = .red
+        videoButton.addTarget(self, action: #selector(videoButtonTapped), for: .touchUpInside)
+        
+        view.addSubview(videoButton)
         videoButton.snp.makeConstraints { make in
-            make.size.equalTo(50)
+            make.size.equalTo(100)
             make.leading.equalTo(view.snp.leading).inset(50)
             make.centerY.equalTo(view.snp.centerY)
         }
-        videoButton.addTarget(self, action: #selector(videoButtonTapped), for: .touchUpInside)
     }
     
-    @objc func cameraButtonTapped() {
-        print("카메라가 눌렸어요")
+    private func checkCameraPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+            guard granted else { return }
+            DispatchQueue.main.async {
+                self?.configureCamera()
+            }
+        }
     }
     
-    @objc func videoButtonTapped() {
-        print("비디오가 눌렸어요")
+    private func configureCamera() {
+        let session = AVCaptureSession()
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: device)
+            if session.canAddInput(input) {
+                session.addInput(input)
+            }
+            
+            if session.canAddOutput(imageOutput) {
+                session.addOutput(imageOutput)
+            }
+            
+            let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+            previewLayer.videoGravity = .resizeAspectFill
+            
+            view.layer.addSublayer(previewLayer)
+            previewLayer.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+            
+            session.startRunning()
+            self.captureSession = session
+            self.cameraPreviewLayer = previewLayer
+        } catch {
+            print("Error configuring camera:", error.localizedDescription)
+        }
+    }
+    
+    @objc private func cameraButtonTapped() {
+        print("Camera button tapped")
+        capturePhoto()
+    }
+    
+    @objc private func videoButtonTapped() {
+        print("Video button tapped")
+    }
+    
+    private func capturePhoto() {
+        guard let captureSession = captureSession else {
+            print("Capture session is not configured")
+            return
+        }
+        
+        imageOutput.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
+    }
+}
+
+extension CameraTestVC: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard let data = photo.fileDataRepresentation(), let image = UIImage(data: data) else {
+            print("Error processing photo")
+            return
+        }
+        
+        DispatchQueue.main.async {
+            self.captureSession?.stopRunning()
+            let imageView = UIImageView(image: image)
+            imageView.contentMode = .scaleAspectFill
+            imageView.frame = self.view.bounds
+            self.view.addSubview(imageView)
+        }
     }
 }
