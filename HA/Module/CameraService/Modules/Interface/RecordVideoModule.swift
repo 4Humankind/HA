2a3ec12d3b86774a3ecdb935ef6ddc8174ccf534
7과11사이 <dongjunlee222@gmail.com>
2024-05-25 14:20:24 +0900

    HA-17 [FIX]: record module

--- HA/Module/CameraService/Modules/Interface/RecordVideoModule.swift
+++ HA/Module/CameraService/Modules/Interface/RecordVideoModule.swift
@@ -14,58 +14,14 @@ protocol RecordVideoInterface: AnyObject {
     func stopRecording()
 }
 
-class RecordVideoModule: NSObject, RecordVideoInterface, AVCaptureFileOutputRecordingDelegate {
+class RecordVideoModule: NSObject, RecordVideoInterface {
 
     private var videoOutput: AVCaptureMovieFileOutput!
-    private var captureSession: AVCaptureSession!
     private var previewLayer: AVCaptureVideoPreviewLayer!
     
     override init() {
         super.init()
-        setupCaptureSession()
-    }
-    
-    func setupCaptureSession() {
-        captureSession = AVCaptureSession()
-        captureSession.sessionPreset = .photo
         
-        guard let camera = AVCaptureDevice.default(for: .video) else {
-            print("Unable to access the camera.")
-            return
-        }
-        
-        do {
-            let cameraInput = try AVCaptureDeviceInput(device: camera)
-            if captureSession.canAddInput(cameraInput) {
-                captureSession.addInput(cameraInput)
-            } else {
-                print("Unable to add camera input to the session.")
-                return
-            }
-            
-            videoOutput = AVCaptureMovieFileOutput()
-            if captureSession.canAddOutput(videoOutput) {
-                captureSession.addOutput(videoOutput)
-            } else {
-                print("Unable to add movie file output to the session.")
-                return
-            }
-            
-        } catch {
-            print("Error setting up camera input: \(error)")
-            return
-        }
-        
-        DispatchQueue.global(qos: .userInitiated).async {
-            self.captureSession.startRunning()
-        }
-    }
-    
-    func setPreviewLayer(to parentView: UIView) {
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        parentView.layer.addSublayer(previewLayer)
-        previewLayer.frame = parentView.layer.frame
-        previewLayer.connection?.videoOrientation = .portrait
     }
     
     func startRecording(whenTapped button: UIButton) {
@@ -84,38 +40,74 @@ class RecordVideoModule: NSObject, RecordVideoInterface, AVCaptureFileOutputReco
         print("녹화를 중지합니다")
     }
     
-    
-    func saveVideoToPhotoLibrary(_ fileURL: URL) {
-        PHPhotoLibrary.shared().performChanges({
-            PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: fileURL)
-        }) { saved, error in
-            if let error = error {
-                print("Error saving video to photo library: \(error.localizedDescription)")
-            } else if saved {
-                print("Video saved to photo library.")
-                self.fetchLatestVideoAsset()
+    private func createDirectory(named directory: String) -> URL? {
+        let fileManager = FileManager.default
+        if let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let filePath = documentDirectory.appendingPathComponent(directory)
+            if !fileManager.fileExists(atPath: filePath.path) {
+                do {
+                    try fileManager.createDirectory(at: filePath, withIntermediateDirectories: true)
+                } catch {
+                    print("파일 매니저에 저장하지 못했습니다.")
+                }
             }
+            print("Document 다이렉토리는 \(filePath)입니다.")
+            return filePath
+        }
+        return nil
+    }
+    
+    func saveData(_ data: Data, toFileNamed fileName: String, inDirectory directoryName: String) {
+        guard let directoryUrl = createDirectory(named: directoryName) else {
+            print("\(directoryName)가 아직 없습니다.")
+            return
+        }
+        
+        let fileUrl = directoryUrl.appendingPathComponent(fileName)
+        do {
+            try data.write(to: fileUrl)
+            print("데이터가 \(fileUrl) 위치에 저장되었습니다.")
+        } catch {
+            print("데이터가 저장되지 않았습니다. \(error.localizedDescription)")
         }
     }
     
-    func fetchLatestVideoAsset() {
-        let fetchOptions = PHFetchOptions()
-        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-        
-        let fetchResult = PHAsset.fetchAssets(with: .video, options: fetchOptions).firstObject
-        if let latestVideo = fetchResult {
-            print("Fetched latest video asset: \(latestVideo)")
-        } else {
-            print("No video asset found.")
-        }
-    }
+//    func saveVideoToPhotoLibrary(_ fileURL: URL) {
+//        PHPhotoLibrary.shared().performChanges({
+//            PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: fileURL)
+//        }) { saved, error in
+//            if let error = error {
+//                print("Error saving video to photo library: \(error.localizedDescription)")
+//            } else if saved {
+//                print("Video saved to photo library.")
+//                self.fetchLatestVideoAsset()
+//            }
+//        }
+//    }
+//    
+//    func fetchLatestVideoAsset() {
+//        let fetchOptions = PHFetchOptions()
+//        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+//        
+//        let fetchResult = PHAsset.fetchAssets(with: .video, options: fetchOptions).firstObject
+//        if let latestVideo = fetchResult {
+//            print("Fetched latest video asset: \(latestVideo)")
+//        } else {
+//            print("No video asset found.")
+//        }
+//    }
+}
+
+extension RecordVideoModule: AVCaptureFileOutputRecordingDelegate {
     
     func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: (any Error)?) {
         if let error = error {
             print("Error recording movie: \(error.localizedDescription)")
         } else {
             print("Finished recording to: \(outputFileURL)")
-            saveVideoToPhotoLibrary(outputFileURL)
+            // saveVideoToPhotoLibrary(outputFileURL)
+            // self.saveData(<#T##data: Data##Data#>, toFileNamed: outputFileURL, inDirectory: "like so?")
+            
         }
     }
 }
